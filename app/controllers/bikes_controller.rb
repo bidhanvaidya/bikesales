2d123dc6b16540601239869c28a95bc9eabf92c4
Romal <romal@romal-Dell-DM061.(none)>
2013-06-24 21:20:25 +1000

    pagination set to 10 and fixed some errors

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -57,7 +57,7 @@ class BikesController < ApplicationController
       
       
     end
-    @bikees=@bikes.paginate(:page => params[:page], :per_page => 3)
+    @bikees=@bikes.paginate(:page => params[:page], :per_page => 10)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @bikes }
