c436a289a51b001872e9ef02799ad887ee065a98
Romal <romal@romal-Dell-DM061.(none)>
2013-06-29 00:51:05 +1000

    minor code fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,37 @@
 //= require twitter/bootstrap
 
 //= require_tree .
+
+    $(window).resize(function() {
+
+  if ($(window).width() <= 979 && $(window).width() >=600){
+      setTimeout(function() {
+          $("#toggle").trigger('click');
+      },1);}
+
+       
+});
+  if ($(window).width() <= 979 && $(window).width() >=600){
+      setTimeout(function() {
+          $("#toggle").trigger('click');
+      },1);}
+
+     var width= $(window).width()/5
+      
+        $("div.logo").animate({
+    
+    textIndent: width,
+
+    
+  }, 3600 );
+
+
+//Google Analytics 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41955848-1', 'bechnu.com');
+  ga('send', 'pageview');
+
