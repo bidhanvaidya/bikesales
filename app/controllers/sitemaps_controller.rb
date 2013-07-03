693350496fe5d1732054a5a111e48d7f6a2270f3
Romal <romal@romal-Dell-DM061.(none)>
2013-07-03 19:26:07 +1000

    sitemap

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -2,7 +2,7 @@ class SitemapsController < ApplicationController
 
   def show
     # Redirect to CloudFront and S3
-    redirect_to "https://s3.amazonaws.com/sitemap_development/sitemaps/sitemap.xml.gz"
+    redirect_to "https://s3.amazonaws.com/Sitemap/sitemaps/sitemap.xml.gz"
   end
 
 end
